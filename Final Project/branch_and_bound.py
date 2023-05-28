81546b2aca6b27d4eeaa1e354dd982c61ebe055a
WeslleyDeziderio <weslleydeziderio@gmail.com>
2023-05-28 16:03:00 -0300

    feat: initial implementation for the algorithm

--- Final Project/branch_and_bound.py
+++ Final Project/branch_and_bound.py
@@ -1,11 +1,33 @@
 from utils.read_file import *
 from utils.create_model import *
 
-import sys
+import numpy as np
+import copy
 
-def main():
-    print("Hello, world")
-    create_model()
+BRANCHING_RULE = 0.5
+
+is_integer = lambda x: x % 2 == 0
+distance_to_node = lambda x: abs(x-BRANCHING_RULE)
+model = create_model()
+
+def solve_model(model):
+    _ = model.optimize()
+
+    return model
+
+def main(nodes, model, dual_limit, primal_limit):
+    dual_limit = np.inf
+    primal_limit = np.inf
+    leafs = [] 
+
+    for i, node_item in enumerate(nodes):
+        node: Model = node_item.copy()
+
+        try:
+            solve_model(node)
+            dual_limit = min(dual_limit, node.objective_value)
+        except:
+            continue
 
 if __name__ == "__main__":
     main()
