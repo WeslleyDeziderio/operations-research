8fa7d8471602fe2b8a9673db2760c82deb8b9964
WeslleyDeziderio <weslleydeziderio@gmail.com>
2023-06-03 20:30:46 -0300

    refactor: update utils

---
+++ project/utils/create_model.py
@@ -0,0 +1,17 @@
+from mip import Model, xsum, BINARY, MAXIMIZE, CBC
+from utils.read_file import *
+
+def create_model():
+    instance = read_instance(sys.argv[1])
+    instance.print_instance()
+
+    model = Model(sense=MAXIMIZE, solver_name=CBC)
+    x = [model.add_var(var_type=BINARY, name=f"x_{i}") for i in range(instance.num_vars)]
+    model.objective = xsum(instance.objective_function[i] * x[i] for i in range(instance.num_vars))
+
+    for i in instance.constraints:
+        model += xsum(i[j] * x[j] for j in range(instance.num_vars)) <= i[-1]
+
+    model.write("model.lp")
+
+    return model
---
+++ project/utils/read_file.py
@@ -0,0 +1,37 @@
+import sys
+import numpy as np
+
+class Instance:
+    def __init__(self, num_vars, num_constraints, objective_function, constraints):
+        self.num_vars = num_vars
+        self.num_constraints = num_constraints
+        self.objective_function = objective_function
+        self.constraints = constraints
+
+    def print_instance(self):
+        print("Instance:", sys.argv[1])
+        print("Variables:", self.num_vars)
+        print("Constraints:", self.num_constraints)
+        print("max:", self.objective_function)
+        print("S.a:")
+        for constraint in self.constraints:
+            print(constraint)
+
+def read_instance(instance_path):
+    try:     
+        with open(instance_path) as file:
+            num_vars, num_constraints = map(int, file.readline().split())
+            objective_function = [int(c) for c in file.readline().split()]
+
+            constraints = []
+            for _ in range(num_constraints):
+                line = file.readline()
+                constraints.append([int(c) for c in line.split()])
+            return Instance(num_vars, num_constraints, objective_function, constraints)
+
+    except FileNotFoundError:
+        print(f"Error: File '{instance_path}' not found.")
+    except (ValueError, IndexError):
+        print(f"Error: Invalid instance file format in '{instance_path}'.")
+
+    return None
